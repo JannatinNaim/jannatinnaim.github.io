ca0f6d33d12e6bcaa25da1d6f652b69eca739d0c
MD Jannatin Naim <contact.jannatinnaim@gmail.com>
2022-03-26 22:34:04 +0600

    [section] home / featured blogs (#5)
    Featured Blogs section.

--- docs/js/home.js
+++ docs/js/home.js
@@ -28,3 +28,34 @@ function togglePageHeaderNavigationToggle() {
 }
 
 togglePageHeaderNavigationToggle();
+
+function rotateFeaturedBlogsCarousel() {
+  const featuredBlogsCarouselNavigationButtons = document.querySelectorAll(
+    ".featured_blogs .carousel .carousel__button"
+  );
+
+  featuredBlogsCarouselNavigationButtons.forEach(function (button) {
+    button.addEventListener("click", function () {
+      const blogPosts = document.querySelector(".featured_blogs .blogs");
+      const activeBlog = document.querySelector(".featured_blogs .blogs .blog.blog--active");
+
+      let currentIndex = [...blogPosts.children].indexOf(activeBlog);
+
+      if (button.classList.contains("carousel__button--next")) {
+        currentIndex += 1;
+      }
+
+      if (button.classList.contains("carousel__button--prev")) {
+        currentIndex -= 1;
+      }
+
+      if (currentIndex === blogPosts.children.length) currentIndex = 0;
+      if (currentIndex < 0) currentIndex = blogPosts.children.length - 1;
+
+      blogPosts.children[currentIndex].classList.add("blog--active");
+      activeBlog.classList.remove("blog--active");
+    });
+  });
+}
+
+rotateFeaturedBlogsCarousel();
