a51c3d9b5ebb5416c6036148f85f2f42bbda01e0
MD Jannatin Naim <contact.jannatinnaim@gmail.com>
2022-03-30 18:54:20 +0600

    [feat] home / featured blogs auto navigator
    Featured blogs section auto rotator.

--- docs/js/home.js
+++ docs/js/home.js
@@ -2,8 +2,10 @@ import toggleMobileNavigation from "./home/toggleMobileNavigation.js";
 import featuredBlogsCarouselNavigation from "./home/featuredBlogsCarouselNavigation.js";
 import featuredBlogsCarouselCardsIndexGenerator from "./home/featuredBlogsCarouselCardsIndexGenerator.js";
 import featuredBlogsCarouselCardsIndexUpdater from "./home/featuredBlogsCarouselCardsIndexUpdater.js";
+import featuredBlogsCarouselAutoNavigator from "./home/featuredBlogsCarouselAutoNavigator.js";
 
 toggleMobileNavigation();
 featuredBlogsCarouselNavigation();
 featuredBlogsCarouselCardsIndexGenerator();
 featuredBlogsCarouselCardsIndexUpdater();
+featuredBlogsCarouselAutoNavigator();
