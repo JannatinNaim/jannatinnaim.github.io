82dfababca4c2d32afdac877b45cb72372d7239c
MD Jannatin Naim <contact.jannatinnaim@gmail.com>
2022-03-31 21:31:13 +0600

    [section] home / technologies
    Technologies section.

--- docs/js/home.js
+++ docs/js/home.js
@@ -3,9 +3,11 @@ import featuredBlogsCarouselNavigation from "./home/featuredBlogsCarouselNavigat
 import featuredBlogsCarouselCardsIndexGenerator from "./home/featuredBlogsCarouselCardsIndexGenerator.js";
 import featuredBlogsCarouselCardsIndexUpdater from "./home/featuredBlogsCarouselCardsIndexUpdater.js";
 import featuredBlogsCarouselAutoNavigator from "./home/featuredBlogsCarouselAutoNavigator.js";
+import technologiesWheel from "./home/technologiesWheel.js";
 
 toggleMobileNavigation();
 featuredBlogsCarouselNavigation();
 featuredBlogsCarouselCardsIndexGenerator();
 featuredBlogsCarouselCardsIndexUpdater();
 featuredBlogsCarouselAutoNavigator();
+technologiesWheel();
